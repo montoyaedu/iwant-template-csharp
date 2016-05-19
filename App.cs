4baa40d4111b7dd9eeefd4ddf332eb4d154f8959
Montoya Edu <montoya.edu@gmail.com>
2016-05-19 12:27:09 +0200

    Initial commit

---
+++ App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ${RootNamespace}
+{
+    static class ${AssemblyName}
+    {
+        public static void Main (string[] args)
+        {
+            foreach (var arg in args)
+            {
+                if (arg == "HelloWorld")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ AppTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace ${RootNamespace}
+{
+    [TestFixture]
+    public class ${AssemblyName}Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            ${AssemblyName}.Main(new []{"HelloWorld"});
+            Assert.IsTrue(true);
+        }
+    }
+}
